db19a693742a804a7abb3ce4f8169d54519dd612
Jonny Walter <jonny.walter@ownit.nu>
2023-02-26 23:43:54 +0100

    Add project files.

---
+++ IndustrialMachinepark/Program.cs
@@ -0,0 +1,16 @@
+using IndustrialMachinepark;
+using IndustrialMachinepark.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//This HttpClient is for the Weather Forecast  
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//This HttpClient is for the IndustrialMachinepark
+builder.Services.AddHttpClient<IMachineparkClient, MachineparkClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+await builder.Build().RunAsync();
---
+++ IndustrialMachinepark/Services/IMachineparkClient.cs
@@ -0,0 +1,10 @@
+﻿using IndustrialMachinepark.Shared.Entities;
+
+namespace IndustrialMachinepark.Services
+{
+    public interface IMachineparkClient
+    {
+        Task<IEnumerable<Machine>> GetAsync();
+        Task<IEnumerable<Machine>> GetJsonAsync<Machine>();
+    }
+}
